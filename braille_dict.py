d48f6ee57d63d6c7cf9486726e9d0518e730cd6b
michael-hart <michael.hart1994@gmail.com>
2015-01-26 21:36:49 +0000

    Added braille dictionary file containing two definitions

---
+++ braille_dict.py
@@ -0,0 +1,8 @@
+# File created 26/01/2015
+# Contains a single variable, braille_dict, which can be used for the 
+# conversion between braille characters and Braille lights
+
+braille_dict = {
+	'a': [True, True, True, True, True, True],
+	' ': [False, False, False, False, False, False]
+}
