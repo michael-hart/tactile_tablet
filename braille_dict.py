961f2223791ff6b6c2caf15bd5eec5a3445cc135
michael-hart <michael.hart1994@gmail.com>
2015-01-26 22:02:49 +0000

    Changed character conversion to return nested loops

--- braille_dict.py
+++ braille_dict.py
@@ -1,8 +1,9 @@
 # File created 26/01/2015
 # Contains a single variable, braille_dict, which can be used for the 
 # conversion between braille characters and Braille lights
+# Nested lists allow for single list return values for all characters
 
 braille_dict = {
-	'a': [True, True, True, True, True, True],
-	' ': [False, False, False, False, False, False]
+	'a': [[True, True, True, True, True, True]],
+	' ': [[False, False, False, False, False, False]]
 }
--- tablet.py
+++ tablet.py
@@ -6,6 +6,7 @@
 import RPi.GPIO as GPIO
 import time
 import atexit
+from braille_dict import braille_dict
 
 led_pins = [12, 16, 18, 22, 24, 26]
 
@@ -17,10 +18,12 @@ def main():
 	atexit.register(cleanup)
 	print "Enter sentences for Braille display"
 	while True:
-		display_str = raw_input('')
+		display_str = raw_input('-> ')
 		for c in display_str:
 			display_character(c)
-			time.sleep(0.5)
+			time.sleep(0.9)
+			display_character(' ')
+			time.sleep(0.1)
 		# Clear single character display
 		display_character(' ')
 
@@ -28,18 +31,25 @@ def cleanup():
 	print "Cleaning up..."
 	GPIO.cleanup()
 
-def display_character(c):
-	""" Given a character, c, displays the Braille equivalent on
+def display_string(s):
+	""" Given a string,sc, displays the Braille equivalent on
 	the LEDs with the global pin values. """
-	assert type(c) == str and len(c) == 1
-	for pin, val in zip(led_pins, char_to_braille(c)):
+	assert type(s) == str
+	# Buffer for complete conversion of string
+	buf = []
+	for c in s:
+		buf += char_to_braille(c)
+	for pin, val in zip(led_pins, buf):
 		GPIO.output(pin, val)
 
 def char_to_braille(c):
 	""" Given a character, c, returns a list of six bool values
 	for display on the LEDs """
 	assert type(c) == str and len(c) == 1
-	return [True]*6
+	if braille_dict.has_key(c):
+		return braille_dict[c]
+	else:
+		return [[False]*6]
 
 if __name__ == '__main__':
 	main()
